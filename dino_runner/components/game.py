4da39a1683b05baf66d71fac557456443e22b472
Trapoot <jsantamaria131312@gmail.com>
2023-03-30 21:40:58 -0500

    extra_class

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,6 +2,7 @@ import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.Cactus import Cactus
 
 class Game:
     def __init__(self):
@@ -15,6 +16,7 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosaur()
+        self.enemy = Cactus()
 
     def run(self):
         # Game loop: events - update - draw
@@ -31,13 +33,15 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        self.player.update()
+        self.enemy.update()
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
+        self.enemy.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
